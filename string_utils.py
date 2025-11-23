7426e6fa28719063e2d80ed7840903f14db4c986
netaashkenazi <neta.ashkenazi.email@gmail.com>
2025-11-23 08:54:07 +0200

    Update string_utils.py

--- string_utils.py
+++ string_utils.py
@@ -1,25 +1,48 @@
 
 
 
-def split_before_uppercases(formula):
-    pass  # replace the pass with your code
+def split_before_each_uppercases(formula):
+    start = 0 
+    end = 0
+    i = 0
+    split_formula = []
+    if not formula:
+        return []
+    for i in range(1,len(formula)):
+      end = i
+      if formula[i].isupper() : 
+       split_formula.append(formula[start:end])
+       start = end
+    split_formula.append(formula[start:])
+    return split_formula
 
-def split_at_digit(formula):
-    pass  # replace the pass with your code
+def split_at_first_digit(formula):
+   digit_location = 1
+   new_formula = ()
+   for char in formula[1:]:
+      if char.isdigit():
+          break
+      digit_location += 1
 
-def count_atoms_in_molecule(molecular_formula):
-    """Takes a molecular formula (string) and returns a dictionary of atom counts.  
-    Example: 'H2O' → {'H': 2, 'O': 1}"""
+   if digit_location == len(formula):
+       return formula,1
+   else :
+      figure = int(formula[digit_location:])
+      return formula[:digit_location],figure
 
-    # Step 1: Initialize an empty dictionary to store atom counts
+   
+       
+def count_atoms_in_molecule(formula):
+    atoms_count_dict = {}
+    
 
-    for atom in split_by_capitals(molecular_formula):
-        atom_name, atom_count = split_at_number(atom)
+    for atom in split_before_each_uppercases(formula):
+        atom_name, atom_count = split_at_first_digit(atom)
         
-        # Step 2: Update the dictionary with the atom name and count
 
-    # Step 3: Return the completed dictionary
+        atoms_count_dict[atom_name] = atom_count
 
+    return atoms_count_dict
 
 
 def parse_chemical_reaction(reaction_equation):
@@ -36,3 +59,4 @@ def count_atoms_in_reaction(molecules_list):
     for molecule in molecules_list:
         molecules_atoms_count.append(count_atoms_in_molecule(molecule))
     return molecules_atoms_count
+
